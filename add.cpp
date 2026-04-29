e0e068c8fa188986d527438243910333fd5fd1ba
achyuth <achyuthgalla@gmail.com>
2026-04-29 11:56:17 -0400

    file modified

---
+++ add.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    a=8;
+    b=9;
+
+    int arr[9] = {2,3,5,6,8,9,42,15,16};
+
+    if(a>b)
+    {
+        cout<<"a is greater than b";
+    }
+    else{
+        cout<<"a is not greater than b";
+    }
+
+    for(int i=0; i<sizeof(a); i++){
+        cout<<i;
+    }
+    return 0;
+}
